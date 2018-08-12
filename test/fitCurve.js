c95f7a35997c5d06b6a9917239cdbfa82349016f
Paul Walker <starsinmypockets@gmail.com>
2018-08-12 12:28:18 -0400

    Test stub for fitCurve

---
+++ test/fitCurve.js
@@ -0,0 +1,9 @@
+const simple = require('../fitCurve.js')
+const assert = require('chai').assert
+let simpleScore1, simpleScore2
+
+describe('Tests run', function () {
+  it('Should be true', function () {
+    assert(true)
+	})
+})
